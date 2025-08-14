7d3f962dbab09e34ac21f18b5b544046734cc025
Kang-minjong <kjskkdkmj@gmail.com>
2025-08-14 14:24:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,21 +13,21 @@ train_input, test_input, train_target, test_target = train_test_split(
     train_set, pH, shuffle=False, random_state=42    
 )
 
-ss = StandardScaler()
-train_scaled = ss.fit_transform(train_input)
-test_scaled = ss.transform(test_input)
-
 poly = PolynomialFeatures(degree=2, include_bias=False)
-train_poly = poly.fit_transform(train_scaled)
-test_poly = poly.transform(test_scaled)
+train_poly = poly.fit_transform(train_input)
+test_poly = poly.transform(test_input)
+
+ss = StandardScaler()
+train_scaled = ss.fit_transform(train_poly)
+test_scaled = ss.transform(test_poly)
 
 # 엘라스틱넷과 랜덤 포레스트 모델 생성 및 훈련
-elastic_model = train_elasticnet(train_poly, train_target)
+elastic_model = train_elasticnet(train_scaled, train_target)
 rf_model = train_random_forest(train_input, train_target)
 
 # 성능 결과
 print("\nElasticNet 성능:")
-evaluate_model(elastic_model, train_poly, train_target, test_poly, test_target)
+evaluate_model(elastic_model, train_scaled, train_target, test_scaled, test_target)
 
 print("\nRandom Forest 성능:")
 evaluate_model(rf_model, train_input, train_target, test_input, test_target)
